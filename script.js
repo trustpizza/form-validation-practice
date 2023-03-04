4bb3fa07b46ca727466c30c6146709cc9d78cf0b
trustpizza <maxelolsson@gmail.com>
2023-03-04 15:33:34 -0600

    Add custom pattern

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ form.addEventListener('submit', (e) => {
 email.addEventListener('input', (e) => {
     if (email.validity.typeMismatch) {
         email.setCustomValidity("Email is a required field stupid!")
+    } else if (email.validity.patternMismatch) {
+        email.setCustomValidity("Email needs to be in email@website.com format")
     } else {
         email.setCustomValidity("")
     }
